15c05c1d6a1f93939b004a01515b3e949a68a355
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-11-20 12:22:35 +0100

    simplify code of the server

--- src/imf-mm-api-server.js
+++ src/imf-mm-api-server.js
@@ -19,45 +19,33 @@ const _module = require('path').basename(__filename)
 
 let server = new Koa();
 
-server.mm_init = async function (option) {
-    return db.init()
-        .then(() => {
-            //load the utility functions
-            var u = require('./lib/util')
+//log access if required
+if (config.get('log_options').log_api_access) {
+    server.use(pino)
+}
 
-            //log access if required
-            if (config.get('log_options').log_api_access) {
-                server.use(pino)
-            }
+//load the body parser
+server.use(bodyParser())
 
-            //load the body parser
-            server.use(bodyParser())
+if (config.get('enable.www')) {
+    let www = require('koa-static')
+    server.use(mount(config.get('mount_point'), www('docs/www/', {})))
+}
+if (config.get('enable.admin')) {
+    let api = require('./api-admin.js')
+    server.use(mount(config.get('mount_point'), api.routes()))
+}
+if (config.get('enable.assets')) {
+    let api = require('./api-assets.js')
+    server.use(mount(config.get('mount_point'), api.routes()))
+}
+if (config.get('enable.crawl')) {
+    let api = require('./api-crawl-fs.js')
+    server.use(mount(config.get('mount_point'), api.routes()))
+}
 
-            //load all the server modules depending on what was configured
-            if (config.get('enable.www')) {
-                let www = require('koa-static')
-                server.use(mount(config.get('mount_point'), www('docs/www/', {})))
-            }
-            if (config.get('enable.admin')) {
-                let api = require('./api-admin.js')
-                server.use(mount(config.get('mount_point'), api.routes()))
-            }
-            if (config.get('enable.assets')) {
-                let api = require('./api-assets.js')
-                server.use(mount(config.get('mount_point'), api.routes()))
-            }
-            if (config.get('enable.crawl')) {
-                let api = require('./api-crawl-fs.js')
-                server.use(mount(config.get('mount_point'), api.routes()))
-            }
-        })
-        .catch((e) => {
-            log.error(`${rJ('server did not init: ')}${e}`)
-            log.error(`${rJ('NODE_ENV mode: ')}${process.env.NODE_ENV}`)
-            log.error(`${rJ('using db: ')}${config.get('database.type')}`)
-            log.error(`${rJ('will use  port: ')}${config.get('port')}`)
-            process.exit(1)
-        })
+server.mm_init = async (resolve) => {
+    db.init()
 }
 
 module.exports = server
